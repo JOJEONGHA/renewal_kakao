00b9da56b839d15b75205a40efdb7668111dbc59
jeongha <wosd1233@gmail.com>
2020-04-22 00:41:28 +0900

    All frame clear

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,19 @@
 $(function(){
-    // let text = obj.menu[0];
-    // console.log(obj.menu.key[0]);
-    // console.log(obj.menu[1]);
-    // console.log(obj.menu[2]);
+    $(".main_cont").on("mousewheel DOMMouseScroll",function(e){
+        e.preventDefault();
+        var delta = 0;
+        if (!event) event = window.event;
+        if (event.wheelDelta) {
+            delta = event.wheelDelta / 120;
+            if (window.opera) delta = -delta;
+        } 
+        else if (event.detail)
+            delta = -event.detail / 3;
+      
+        if (delta < 0) {
+            $(".main_cont").stop().animate({scrollLeft: $(".main_cont").scrollLeft() + 50}, 0, "linear");
+        } else {
+            $(".main_cont").stop().animate({scrollLeft: $(".main_cont").scrollLeft() - 50},  0, "linear");
+        }
+    })
 })
