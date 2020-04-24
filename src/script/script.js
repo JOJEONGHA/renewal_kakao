fbe00eb139ac65f1e44ab80c138840a65645da22
jeongha <wosd1233@gmail.com>
2020-04-24 20:26:55 +0900

    Fixed scroll area

--- src/script/script.js
+++ src/script/script.js
@@ -1,5 +1,5 @@
 $(function(){
-    $(".main_cont").on("mousewheel DOMMouseScroll",function(e){
+    $(".main_cont .main_wrap").on("mousewheel DOMMouseScroll",function(e){
         e.preventDefault();
         var delta = 0;
         if (!event) event = window.event;
@@ -9,11 +9,11 @@ $(function(){
         } 
         else if (event.detail)
             delta = -event.detail / 3;
-      
+        
         if (delta < 0) {
             $(".main_cont").stop().animate({scrollLeft: $(".main_cont").scrollLeft() + 50}, 0, "linear");
         } else {
-            $(".main_cont").stop().animate({scrollLeft: $(".main_cont").scrollLeft() - 50},  0, "linear");
+            $(".main_cont").stop().animate({scrollLeft: $(".main_cont").scrollLeft() - 50}, 0, "linear");
         }
     })
 })
