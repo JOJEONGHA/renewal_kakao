08098d24461d345b8b99df813c37e5e900f626f7
jeongha <wosd1233@gmail.com>
2020-05-03 14:23:30 +0900

    Working main text animation

--- src/script/main_slider.js
+++ src/script/main_slider.js
@@ -7,6 +7,7 @@ $(function(){
     lbtn.click(function(){
         mimg.each(function(index,ele){
             mclc(index,ele,1,amount);
+            
         })
     })
     rbtn.click(function(){
@@ -31,5 +32,25 @@ function mclc(index,ele,dir,amount){
         num = 0;
 
     $(ele).addClass("dp" + num);
+    // text 설정
+    // 1. dp1 해당 클래스 ty 뒤 숫자 찾기
+    // 2. 해당 숫자에 맞는 json text 찾기
+    // 3. text tag 찾기
+    // 4. tag에 글자 설정 및 animation 리셋
+    if(num == 1){
+        let ty_order = class_txt.indexOf("ty");
+        ty_order = class_txt.substr(ty_order+2,1);
+
+        let pmtag = $(".main_cont .main_wrap .int_cont .int_wrap > a > .txt_wrap"), 
+            mtag = pmtag.children(".txt_tit"),
+            mtxt = obj.main_txt["page" + ty_order];
+        mtag.html(mtxt);
+
+        mtag.removeClasss("on");
+        void mtag.offsetWidth;
+        mtag.addClasss("on");
+        // var clone_tag = mtag.cloneNode(true);
+        // pmtag.replaceChild(clone_tag, mtag);
+    }
 }
 
